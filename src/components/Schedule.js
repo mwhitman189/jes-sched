89a0ec22fb42fd7849faa001cf9d0be25f12baea
mWhitman189 <mwhitman189@gmail.com>
2019-12-05 18:30:31 +0900

    feat: Add teaching minutes calculation and display

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,21 +3,38 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import moment from "moment";
 import { WorkWeek } from "./CustomView";
-import myEventsList from "../events";
+import eventsList from "../events";
+import teachersList from "../teachers";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 
 const Schedule = props => {
-  const [events, setEvents] = useState(myEventsList);
-  console.log(myEventsList);
+  const [events, setEvents] = useState(eventsList);
+  const [teacherData, setTeacherData] = useState(teachersList);
+  console.log(teacherData);
 
   // Limit displayed hours of the day
   const minTime = new Date();
   minTime.setHours(9, 0, 0);
   const maxTime = new Date();
   maxTime.setHours(21, 0, 0);
+
+  const addTeachingMins = () => {
+    events.forEach(e => {
+      for (let teacher in teacherData) {
+        console.log(teacher);
+        if (parseInt(teacher) === e.resourceId) {
+          teacherData[teacher].teachingMins += e.duration;
+          setTeacherData([...teacherData]);
+        }
+        teacherData[
+          teacher
+        ].resourceTitle = `${teacherData[teacher].name} ${teacherData[teacher].teachingMins}`;
+      }
+    });
+  };
 
   // Style events based on event.type
   const eventStyleGetter = event => {
@@ -54,6 +71,7 @@ const Schedule = props => {
 
   return (
     <div>
+      <button onClick={addTeachingMins}>ClickMe</button>
       <DragAndDropCalendar
         style={{ width: "95vw", maxHeight: "100%" }}
         localizer={localizer}
@@ -62,12 +80,7 @@ const Schedule = props => {
         events={events}
         startAccessor="start"
         endAccessor="end"
-        resources={[
-          { resourceId: 0, resourceTitle: "Martin" },
-          { resourceId: 1, resourceTitle: "Tim" },
-          { resourceId: 2, resourceTitle: "Sarah" },
-          { resourceId: 3, resourceTitle: "Miles" }
-        ]}
+        resources={teacherData}
         resourceIdAccessor="resourceId"
         resourceTitleAccessor="resourceTitle"
         selectable
---
+++ src/teachers.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    resourceId: 0,
+    resourceTitle: "Martin",
+    name: "Martin",
+    teachingMins: 0
+  },
+  {
+    resourceId: 1,
+    resourceTitle: "Tim",
+    name: "Tim",
+    teachingMins: 0
+  },
+  {
+    resourceId: 2,
+    resourceTitle: "Sarah",
+    name: "Sarah",
+    teachingMins: 0
+  },
+  {
+    resourceId: 3,
+    resourceTitle: "Miles",
+    name: "Miles",
+    teachingMins: 0
+  }
+];
