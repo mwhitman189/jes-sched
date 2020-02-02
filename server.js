6a53e7fe0cd73f6c45ea13aa1e6a848e59faa19c
mWhitman189 <mwhitman189@gmail.com>
2020-02-02 23:29:17 +0900

    fix: Remove potential cause of CORS issue in server.js

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use("/lessons", lessonsRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 app.listen(port, () => {
