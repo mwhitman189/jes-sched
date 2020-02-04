38a821c6809ec7f26acc4e62f2047b20b2509e07
mWhitman189 <mwhitman189@gmail.com>
2020-02-04 09:24:41 +0900

    fix: Add mLab for MongoDB connection

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require("path");
 // require("dotenv").config();
 
 const app = express();
-app.use(cors());
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
-const uri = require("./config/config").ATLAS_URI;
+const uri = process.env.MONGODB_URI || require("./config/config").ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true
@@ -33,9 +33,9 @@ app.use("/lessons", lessonsRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
