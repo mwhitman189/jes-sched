2e1997b2112d999c5b483a5ed582cafdd15ab573
mWhitman189 <mwhitman189@gmail.com>
2020-05-22 09:18:03 +0900

    feat: Add https redirect

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
-const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser("05047dsf$16*60"));
 app.use(helmet());
 
 const uri = process.env.MONGODB_URI || require("./config/config").ATLAS_URI;
@@ -40,9 +38,17 @@ app.use("/api/students", studentsRouter);
 app.use("/api/lessons", lessonsRouter);
 app.use("/api/payments", paymentsRouter);
 
+const forceSsl = function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.direct(["https://", req.get("Host"), req.url].join(""));
+  }
+  return next();
+};
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.use(forceSsl);
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
