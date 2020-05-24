c8ec5f14114d02b106d61b4810141453d4955e81
mWhitman189 <mwhitman189@gmail.com>
2020-05-24 14:27:11 +0900

    fix: Fix ssl redirect issue occurring when additional url page specified

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const forceSsl = function (req, res, next) {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    app.use(forceSsl);
+    app.use(forceSsl(req, res));
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
